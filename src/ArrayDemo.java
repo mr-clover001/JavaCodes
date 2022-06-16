046afd35b1320caf0e025036ad6ff37885beea41
Sumit Kumar Sharma <63010032+mr-clover001@users.noreply.github.com>
2022-06-16 12:57:38 +0530

    String

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,16 +1,43 @@
-public class ArrayDemo {
-    public static void main(String[] args) {
-      int [] arr = {2, 3, 5, 6, 2};
-      int [][] multiDemArr = {
-              {1,3},
-              {2,4},
-              {5,6}
-      };
+import java.util.Scanner;
 
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(" size of first array " + multiDemArr[0].length);
+public class ArrayDemo {
+
+//    public static int ArrReturn(int n){
+//        Scanner sc = new Scanner(System.in);
+//       int [] Ar = new int[n] ;
+//        for (int i=0;i<n;i++)
+//        {
+//            Ar[i]=sc.nextInt();
+//        }
+//
+////        return Ar;
+//
+//    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the Size Array: ");
+        n = sc.nextInt();
+//        int Arr[] = ArrReturn(n);
+
+        for(int i=0; i<n;i++){
+            System.out.println(i+" ");
+        }
+
+
+
+//      int [] arr = {2, 3, 5, 6, 2};
+////      int [][] multiDemArr = {
+////              {1,3},
+////              {2,4},
+////              {5,6}
+////      };
+
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(" size of first array " + multiDemArr[0].length);
 
     }
 }
--- src/stringDemo.java
+++ src/stringDemo.java
@@ -1,14 +1,25 @@
 public class stringDemo {
     public static void main(String[] args) {
-        String str = "Sharma";
-        String str2 = "Sharm2";
-        String replace = str2.replace('2','a');
+        String Str = "Good morning";
+        String str = "Anoop kumar Sharma";
 
-        System.out.println(replace.contains("2"));
-        System.out.println(replace);
-        System.out.println("printing the equality of string "+str.equals(replace));
+//        String Methods
         System.out.println(str.length());
-        System.out.println(str.substring(2));
-        System.out.println(str.compareTo(str2));
+        System.out.println(str.charAt(3));
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(2,6));
+        System.out.println(Str.concat(str));
+        System.out.println(str.indexOf("kumar"));
+
+
+//        String str2 = "Sharm2";
+//        String replace = str2.replace('2','a');
+//
+//        System.out.println(replace.contains("2"));
+//        System.out.println(replace);
+//        System.out.println("printing the equality of string "+str.equals(replace));
+//        System.out.println(str.length());
+//        System.out.println(str.substring(2));
+//        System.out.println(str.compareTo(str2));
     }
 }
