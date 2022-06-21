803e3516ecf1732b8af7918bb92e9ef98ab7ad5c
Sumit Kumar Sharma <63010032+mr-clover001@users.noreply.github.com>
2022-06-21 23:59:52 +0530

    changes

--- src/TestEncapsulation.java
+++ src/TestEncapsulation.java
@@ -33,6 +33,12 @@ class Account {
 
 public class TestEncapsulation {
     public static void main(String[] args) {
-
+        //creating instance of Account class
+        Account acc=new Account();
+        //setting values through setter methods
+        acc.setAcc_no(7560504000L);
+        acc.setName("Sonoo Jaiswal");
+        acc.setEmail("sonoojaiswal@javatpoint.com");
+        acc.setAmount(500000f);
     }
 }
