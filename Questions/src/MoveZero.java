c1dfa6fa8a07b969cd4038c0abd136194a0b0045
Sumit Kumar Sharma <63010032+mr-clover001@users.noreply.github.com>
2022-06-24 22:29:43 +0530

    abstract

--- Questions/src/MoveZero.java
+++ Questions/src/MoveZero.java
@@ -5,15 +5,15 @@ public class MoveZero {
         int[] input = {1, 0, 6, 0, 4};
 
         int j = 0;
-//        for (int i = 0; i < input.length; i++){
-//            if(input[i]!=0){
-//                int temp = input[j];
-//                input[j] = input[i];
-//                input[i] = temp;
-//                j++;
-//
-//            }
-//        }
+        for (int i = 0; i < input.length; i++){
+            if(input[i]!=0){
+                int temp = input[j];
+                input[j] = input[i];
+                input[i] = temp;
+                j++;
+
+            }
+        }
         for (int element : input) {
             System.out.print(element + " ");
         }
--- src/AbstractClassExample.java
+++ src/AbstractClassExample.java
@@ -1,5 +1,8 @@
 abstract class Bank{
     abstract int getRateOfInterest();
+    static  void fuckClass(){
+        System.out.println("come On");
+    }
 }
 class SBI extends Bank{
     int getRateOfInterest(){return 7;}
@@ -10,6 +13,8 @@ class PNB extends Bank{
 
 class AbstractClassExample{
     public static void main(String args[]){
+        Bank c = new SBI();
+        c.fuckClass();
         Bank b;
         b=new SBI();
         System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
