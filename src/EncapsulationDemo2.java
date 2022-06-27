b337f1c3f4b8b6af7a8e5fee70bc37f0543c8012
Sumit Kumar Sharma <63010032+mr-clover001@users.noreply.github.com>
2022-06-27 14:37:44 +0530

    changes

--- src/EncapsulationDemo2.java
+++ src/EncapsulationDemo2.java
@@ -1,7 +1,14 @@
 
-
 public class EncapsulationDemo2 {
+   private String name;
+   private int ID;
+   private int salary;
+
+   public void display(int ID ,int salary , String name){
+       System.out.println(name + " " + ID + " " + salary);
+   }
     public static void main(String[] args) {
 
+       EncapsulationDemo2 en = new EncapsulationDemo2();
     }
 }
