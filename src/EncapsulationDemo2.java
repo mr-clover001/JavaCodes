edbc0f770e9616f86d905c9a8b7372b0b294a543
Sumit Kumar Sharma <63010032+mr-clover001@users.noreply.github.com>
2022-06-27 14:42:04 +0530

    class

--- src/EncapsulationDemo2.java
+++ src/EncapsulationDemo2.java
@@ -1,4 +1,11 @@
+class Employee{
+   private int num ;
+    private String name;
 
+    public void display1(int num, String name){
+        System.out.println(num + " " + name);
+    }
+}
 public class EncapsulationDemo2 {
    private String name;
    private int ID;
@@ -10,5 +17,7 @@ public class EncapsulationDemo2 {
     public static void main(String[] args) {
 
        EncapsulationDemo2 en = new EncapsulationDemo2();
+       en.display(123, 20000, "Anoop sharma");
+
     }
 }
